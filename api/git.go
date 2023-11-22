41d2e2439be39cdf3edb0666dcd0e6890934e984
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-11-22 16:45:40 +0800

    :construction: 发布项目：自动创建标签

--- api/git.go
+++ api/git.go
@@ -1,7 +1,8 @@
 package api
 
 import (
-	"fmt"
+	"github.com/urfave/cli/v2"
+	"github.com/xuxiaowei-com-cn/cicd-release/constant"
 	"log"
 	"os/exec"
 )
@@ -11,11 +12,57 @@ func GitVersion() (string, error) {
 	cmd := exec.Command("git", "--version")
 	output, err := cmd.Output()
 	if err != nil {
-		log.Fatal("无法获取 Git 版本号")
+		log.Printf("无法获取 Git 版本号")
 		return "", err
 	}
 
 	version := string(output)
-	fmt.Println("Git 版本:", version)
+	log.Println("Git 版本:", version)
 	return version, nil
+}
+
+func GitPrintTag(tag string) error {
+	cmd := exec.Command("git", "show", tag, "--no-patch")
+	output, err := cmd.Output()
+	if err != nil {
+		log.Printf("Git 标签 %s 不存在\n", tag)
+		return err
+	}
+
+	log.Printf("Git 标签 %s 内容: \n%s\n", tag, output)
+	return nil
+}
+
+func GitCreateTag(tag string) error {
+	cmd := exec.Command("git", "tag", tag)
+	_, err := cmd.Output()
+	if err != nil {
+		log.Printf("Git 创建标签 %s 异常\n", tag)
+		return err
+	}
+
+	return GitPrintTag(tag)
+}
+
+func AutoCreateTag(context *cli.Context) error {
+	var tag = context.String(constant.Tag)
+	var autoCreateTag = context.Bool(constant.AutoCreateTag)
+
+	err := GitPrintTag(tag)
+	if err != nil {
+		if autoCreateTag {
+			log.Printf("开始 创建标签：%s\n", tag)
+			err = GitCreateTag(tag)
+			if err != nil {
+				return err
+			}
+			log.Printf("完成 创建标签：%s\n", tag)
+
+			return nil
+		} else {
+			return err
+		}
+	} else {
+		return err
+	}
 }
--- api/gitlab.go
+++ api/gitlab.go
@@ -8,5 +8,10 @@ import (
 func Gitlab(prerelease bool, context *cli.Context) error {
 	log.Printf("是否是预发布版本：%v", prerelease)
 
+	err := AutoCreateTag(context)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
